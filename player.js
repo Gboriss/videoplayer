85e369e706fb9e08a70299ab7ccf998843aa0aec
Gboriss <galinaborissenko@gmail.com>
2020-03-08 18:52:37 +0300

    progress

--- player.js
+++ player.js
@@ -112,3 +112,4 @@ progress.addEventListener('click', scrub)
 progress.addEventListener('mousemove', (e) => mousedown && scrub(e))
 progress.addEventListener('mousedown', () => mousedown = true)
 progress.addEventListener('mouseup', () => mousedown = false)
+
